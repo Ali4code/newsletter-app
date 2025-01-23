4b2e05d80e2f1f5bf352a00d0772592c11c8a65c
Ali Hoseini <Ali.hoseini@casumo.com>
2025-01-24 00:27:29 +0330

    more styles on article card

--- src/components/SearchColumn/SearchColumn.tsx
+++ src/components/SearchColumn/SearchColumn.tsx
@@ -6,8 +6,7 @@ export const SearchColumn = ({ apiKeys }: { apiKeys: TApiKeys }) => {
   const [searchFields, setSearchFields] = useState({});
   return (
     <div className={Classes.search_column}>
-      <h2>Search</h2>
-      <div>Search for news articles</div>
+      <div>Search for articles</div>
       <div>
         select your source:
         <input type="text"></input>
