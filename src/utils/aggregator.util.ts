700d413fcb0d9d1f15729d273fa54aa1aaf40ccb
Ali Hoseini <Ali.hoseini@casumo.com>
2025-01-26 14:17:38 +0330

    aggregator fixes

--- src/utils/aggregator.util.ts
+++ src/utils/aggregator.util.ts
@@ -1,4 +1,6 @@
 import { TArticle } from "../components/ArticleRow/ArticleRow";
+import { TPreferences } from "../components/Preferences/Preferences.types";
+import { API_SOURCES } from "../components/SearchColumn/SearchColumn.constants";
 import { TNewsApiOrgResponse } from "../services/NewsApi/NewsApi.types";
 import { TNewYorTimesResponse } from "../services/NewYorkTimes/NewYorkTimes.types";
 import { TGuardianResponse } from "../services/TheGuardian/TheGuardian.types";
@@ -7,15 +9,29 @@ export const getAggregatedNews = ({
   newsApiData,
   newYorkTimesData,
   guardianData,
+  preferences,
 }: {
   newsApiData?: TNewsApiOrgResponse;
   newYorkTimesData?: TNewYorTimesResponse;
   guardianData?: TGuardianResponse;
+  preferences: TPreferences;
 }): TArticle => {
+  const isNewsOrgIncluded = preferences.sources?.includes(
+    API_SOURCES.THE_NEWS_API_ORG.id
+  );
+
+  const isNewYorkTimesIncluded = preferences.sources?.includes(
+    API_SOURCES.NEW_YORK_TIMES.id
+  );
+
+  const isGuardianIncluded = preferences.sources?.includes(
+    API_SOURCES.THE_GUARDIAN.id
+  );
   const aggregated = [
-    ...(newsApiData?.articles || []),
-    ...(newYorkTimesData?.response?.docs || []),
-    ...(guardianData?.response?.results || []),
+    ...((isNewsOrgIncluded && (newsApiData?.articles || [])) || []),
+    ...((isNewYorkTimesIncluded && (newYorkTimesData?.response?.docs || [])) ||
+      []),
+    ...((isGuardianIncluded && (guardianData?.response?.results || [])) || []),
   ];
 
   return aggregated;
--- src/utils/useGetNewsFeed.ts
+++ src/utils/useGetNewsFeed.ts
@@ -56,6 +56,7 @@ export const useGetNewsFeed = ({
     guardianData,
     newsApiData,
     newYorkTimesData,
+    preferences,
   });
 
   return {
