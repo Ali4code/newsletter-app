bddbe5ee61633df4a70fe8d4f2d0ec47c118a0c5
Ali Hoseini <Ali.hoseini@casumo.com>
2025-01-26 19:43:32 +0330

    keys committed

--- src/constants.ts
+++ src/constants.ts
@@ -1,3 +1,10 @@
 export const API_KEYS_LOCAL_STORAGE_KEY = "api_keys";
 
 export const WEBPAGE_STATE_LOCAL_STORAGE_KEY = "webpage_state";
+
+// we should never commit keys to repo, just for the sake of this project i'm gonna commit keys
+export const K_E_Y_S = {
+  newsApiOrg: "6d9ce636a5a44ac5b1ab08a65536fe01",
+  guardianNews: "86ba763a-6e9d-404f-a071-de5794a9a27a",
+  nyTimes: "F2sMCM0oT6GsbPUC8vEWQjx1aeCrB0Ut",
+};
