5fe2058bb1ee5ad0053b77ba5a17d088bb69230c
Ali Hoseini <Ali.hoseini@casumo.com>
2025-01-26 19:51:45 +0330

    link fix to open in new tab

--- src/components/UI/ButtonLink.tsx
+++ src/components/UI/ButtonLink.tsx
@@ -2,7 +2,7 @@ import Classes from "./ButtonLink.module.css";
 
 export const ButtonLink = ({ text, href }: { text: string; href: string }) => {
   return (
-    <a className={Classes.card_btn} href={href}>
+    <a className={Classes.card_btn} href={href} target="_blank" rel="noreferrer">
       {text}
     </a>
   );
